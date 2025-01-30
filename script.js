e27b1db2bdc9262728738812f86e59020f69c41c
Shaiksufiyan157 <122155078+Shaiksufiyan157@users.noreply.github.com>
2025-01-31 01:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function blurry(){
     }
     loading.innerText=`${load}%`;
     loading.style.opacity=scale(load,0,100,1,0);
-    bg.style.filter=`blur(${scale(load,30,100,2,0)}px)`
+    bg.style.filter=`blur(${scale(load,30,100,3,0)}px)`
 }
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
